83d715ca5e63e17d423cb1cc60944960c124b64c
Adhikari Anup <devbyanup@gmail.com>
2026-08-07 17:30:41 +0545

    Paper Scissor rock

---
+++ Day 1/gold_steal.py
@@ -0,0 +1,11 @@
+import random
+print("Welcome to Gold City ")
+Adv=["steal","police"]
+advs=random.choice(Adv)
+type=input("Where do you want to step : Left or Right?").lower()
+print(type)
+if type==advs:
+    print("You found gold")
+else:
+    print("You are caught")
+
