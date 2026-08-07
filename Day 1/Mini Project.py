5decb1a4bc8a424f649e6f3d4c1cfc3b9d13fa97
Adhikari Anup <devbyanup@gmail.com>
2026-08-07 14:58:19 +0545

    Day 1 Pratice of Data Science

---
+++ Day 1/Mini Project.py
@@ -0,0 +1,9 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+country=input("Enter your country:")
+
+print("---------------- Student Information ---------------")
+print(f"Name of the Student:{name}")
+print(f"Age of the Student {age}")
+print(f"Country of the Student {country}")
+
