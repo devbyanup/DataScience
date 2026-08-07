37a663a606a2535d994c2c481b02daffe4c6958d
Adhikari Anup <devbyanup@gmail.com>
2026-08-07 15:15:14 +0545

    Day 1 Pratice of Data Science

---
+++ Day 1/Pratice Questions.py
@@ -0,0 +1,48 @@
+# Print " Hello Python
+from selectors import SelectSelector
+
+print("Hello Python")
+
+# Variable city= "Kathmandu"
+
+print(f"The Name of the City is Kathmandu")
+
+#User name input and print welcome user
+user=input("Enter a User Name:")
+print(f"Welcome {user}")
+
+#User Age Print
+age=int(input("Enter your name:"))
+print(f"Age of user is {age}")
+
+#make a = 20 and b= 30 print it
+a=input(int())
+b=input(int())
+print(f"a is {a} and b is {b}")
+
+# Make a student true using bool
+
+student=bool(input("Is Student True :"))
+if student: True
+print(student)
+
+#store name,age,country in variable
+
+name="Alex"
+print(name)
+age=27
+print(age)
+country="Nepal"
+print(country)
+
+#F string use and write your info
+
+name=input("Enter your name:")
+print(f"My name is {name}")
+age=int(input("Enter your age:"))
+print(f"Age is {age}")
+country=input("Enter your country:")
+print(f"Country is {country}")
+
+#
+
