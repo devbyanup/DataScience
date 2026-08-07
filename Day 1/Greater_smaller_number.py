f8f13c32ca680940728a0cb49235130f1810eb7b
Adhikari Anup <devbyanup@gmail.com>
2026-08-07 20:27:45 +0545

    greater or smaller

---
+++ Day 1/Greater_smaller_number.py
@@ -0,0 +1,12 @@
+print("Find which number is greater or smaller")
+
+n1=int(input("Enter the first number"))
+n2=int(input("Enter the second number"))
+
+if n1>n2:
+    print(n1,"is greater  than",n2)
+elif n1<n2:
+    print(n1,"is   smaller than",n2)
+
+else:
+    print(n1,"is equal with ",n2)
