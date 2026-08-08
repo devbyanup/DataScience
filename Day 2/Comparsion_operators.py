efd767425605f65c014e6fd11e83e677e46c80ba
Adhikari Anup <devbyanup@gmail.com>
2026-08-08 10:34:49 +0545

    Day 2

---
+++ Day 2/Comparsion_operators.py
@@ -0,0 +1,15 @@
+"""
+== equality operators ==
+!= inequality operators !=
+> greater >
+< smaller <
+>= greater or equal
+<= smaller or equal
+"""
+
+x=10
+print(x==10) #true
+print(x!=10) #false
+print(x>10) #false
+print(x>=10) #true
+
