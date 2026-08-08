fa84c4b3a89f91666cf5489786ca2ddc8bcaae7b
Adhikari Anup <devbyanup@gmail.com>
2026-08-08 10:39:55 +0545

    Day 2

---
+++ Day 2/login.py
@@ -0,0 +1,10 @@
+print(" Please enter  useername as a admin  and password as a 12345")
+print(" We are checking logical operators where both conditions should be equal")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == "admin" and password == "12345":
+    print("login success")
+else:
+    print("login failed")
+
+
